7199a4333f50ebccc12ce63ed4182b6a112c2fd2
Howard Huang <h.huang65@gmail.com>
2021-12-15 21:24:18 -0800

    FEATURE: Accept piped STDIN for creating pastes
    The `pst` command should accepted piped data.
    If piped data is detected, any passed in filename is ignored, and the
    piped data is what is uploaded as content for the paste.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,8 +1,14 @@
 package cli
 
 import (
+	"bufio"
+	"flag"
+	"io"
+	"os"
+
 	"git.sr.ht/~hwrd/pst/internal/paste"
 	"git.sr.ht/~hwrd/pst/internal/util"
+	log "github.com/sirupsen/logrus"
 )
 
 type Opts struct {
@@ -12,8 +18,44 @@ type Opts struct {
 }
 
 func Start(opts Opts) {
-	content, err := util.LoadFile(opts.Path)
+	info, err := os.Stdin.Stat()
+	log.WithFields(log.Fields{
+		"mode":          info.Mode(),
+		"ModeNamedPipe": os.ModeNamedPipe,
+		"size":          info.Size(),
+	}).Debug("STDIN info")
 	util.CheckError(err)
+
+	var content string
+
+	if (info.Mode() & os.ModeNamedPipe) != 0 {
+		log.Debug("Receiving input from STDIN")
+		var output []rune
+		reader := bufio.NewReader(os.Stdin)
+
+		for {
+			input, _, err := reader.ReadRune()
+			if err != nil && err == io.EOF {
+				break
+			}
+			util.CheckError(err)
+			output = append(output, input)
+		}
+
+		content = string(output)
+	} else if opts.Path != "" {
+		log.WithFields(log.Fields{
+			"path": opts.Path,
+		}).Debug("Reading content from file")
+
+		content, err = util.LoadFile(opts.Path)
+		util.CheckError(err)
+	} else {
+		log.Debug("Did not receive input from STDIN, nor a filepath")
+
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	paste.Create(opts.Name, opts.Visibility, content)
 }
